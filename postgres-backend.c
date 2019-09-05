29f8aade7ffa5ecd064e3e68d6396c4d9a2827ed
BombeHub <liliang@stumail.neu.edu.cn>
2019-09-05 10:53:51 +0800

    Update postgres-backend.c

--- postgres-backend.c
+++ postgres-backend.c
@@ -9,7 +9,9 @@ main(){  //程序入口
 					exec_simple_query(){     // 执行DML查询
 						start_xact_command()
 						pg_parse_query(){
-							raw_parser()    // parse
+							raw_parser(){    // parse
+
+							}
 						}
 						pg_analyze_and_rewrite(){
 							parse_analyze(){
@@ -26,32 +28,37 @@ main(){  //程序入口
 						pg_plan_queries(){
 							pg_plan_query(){
 								standard_planner(){
-									PlannerInfo subquery_planner(){
-										SS_process_ctes()
-										pull_up_sublinks()
-										pull_up_subqueries()
-										flatten_simple_union_all()
-										preprocess_rowmarks()
+									PlannerInfo subquery_planner(){   //优化入口
+										SS_process_ctes()   //cte 语法处理
+										pull_up_sublinks()   // 上提子连接 （any exist 从句）
+										pull_up_subqueries()  // 上提子查询
+										flatten_simple_union_all()  // 优化sql中union all操作
+										preprocess_rowmarks() 
 										expand_inherited_tables()
-										preprocess_expression()
+										preprocess_expression()  // 条件化简
 										preprocess_qual_conditions()
 										remove_useless_groupby_columns()
 										reduce_outer_joins()
-										grouping_planner(){
-											query_planner(){
-												deconstruct_jointree()	
+										grouping_planner(){    // 处理非SPJ语句并生成物理计划
+											query_planner(){  
+												setup_simple_rel_arrays()  // 找到所有基表
+												add_base_rels_to_query()   // 生成所有基表的优化信息RelOptInfo
+												build_base_rel_tlists()    // RelOptInfo绑定目标列和条件从句
+												deconstruct_jointree()	 // 【重要】join tree分解拉平
 												make_outerjoininfo()
-												RelOptInfo make_one_rel(PlannerInfo){
-													set_rel_size(){
-														cost_qual_eval()
-													}
-													set_rel_pathlist(){
+												RelOptInfo make_one_rel(PlannerInfo){  // 【重要】物理优化, 生成路径
+													set_base_rel_sizes(){   // RangeTblEntry选择率估计等
+														set_rel_size(){
+															set_baserel_size_estimates()
+														}
+													}													
+													set_base_rel_pathlist(){  //针对RangeTblEntry的单表查询路径选择问题：顺序扫描，索引扫描或tid扫描
 														set_plain_rel_pathlist(){
-															add_path(create_seqscan_path())
+															set_cheapest()    //选取三种路径最优的一种
 														}
 													}
-													make_rel_from_joinlist(){
-
+													make_rel_from_joinlist(){   // 多表join的路径选择： 动态规划或遗传算法
+														standard_join_search()
 													}
 												}											
 											}
@@ -60,7 +67,21 @@ main(){  //程序入口
 										SS_charge_for_initplans()
 									}
 									create_plan(PlannerInfo){
-										create_scan_plan()
+										create_scan_plan(){   // 根据path生成对应的plan tree
+											create_seqscan_plan()
+											create_samplescan_plan()
+											create_indexscan_plan()
+											create_bitmap_scan_plan()
+											create_tidscan_plan()
+											create_subqueryscan_plan()
+											create_functionscan_plan()
+											create_tablefuncscan_plan()
+											create_valuesscan_plan()
+											create_ctescan_plan()
+											create_namedtuplestorescan_plan()
+											create_worktablescan_plan()
+											......
+										}
 										create_join_plan()
 									}
 
@@ -73,14 +94,20 @@ main(){  //程序入口
 
 						CreatePortal()
 						PortalDefineQuery()
-						PortalStart()
+						PortalStart() {
+							CreateQueryDesc(){
+								ExecutorStart(){
+									//
+								}
+							}
+						}
 
 
 
 
 						PortalRun(){
 							PortalRunSelect(){
-								standard_executorRun(){
+								standard_executorRun(){   // 执行器入口
 									executePlan(){
 										ExecProcNode(){
 											ExecScan(){
@@ -88,7 +115,7 @@ main(){  //程序入口
 													......
 												}
 											}
-										}
+										} 
 									}
 								}
 							}
